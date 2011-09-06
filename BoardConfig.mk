27b3d188c80ad51679a5c829a6c69b39884887d8
Tom Giordano <tomgiordano83@gmail.com>
2011-09-06 21:08:11 +1000

    blade: update kernel
    update kernel to 2.6.35.7
    update modules
    update sensor lib to work with newer kernel Taos module.
    update copybit
    enable RGBX8888
    
    Change-Id: I399a1c1fd4552f4f480641ae434c124d487e8168

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,13 +26,11 @@ USE_CAMERA_STUB := false
 
 BOARD_HAS_FLIPPED_SCREEN := true
 
-BOARD_NO_RGBX_8888 := true
-
 TARGET_NO_BOOTLOADER := true
 
 TARGET_PREBUILT_RECOVERY_KERNEL := device/zte/blade/recovery_kernel
 
-BOARD_KERNEL_CMDLINE := androidboot.hardware=blade console=null g_android.product_id=0x1354 g_android.serial_number=Blade-CM7
+BOARD_KERNEL_CMDLINE := androidboot.hardware=blade console=null
 
 TARGET_BOARD_PLATFORM := msm7k
 TARGET_ARCH_VARIANT := armv6-vfp
@@ -57,7 +55,7 @@ WIFI_DRIVER_MODULE_NAME     := ar6000
 WITH_JIT := true
 ENABLE_JSC_JIT := true
 
-TARGET_LIBAGL_USE_GRALLOC_COPYBITS := true
+#TARGET_LIBAGL_USE_GRALLOC_COPYBITS := true
 
 JS_ENGINE := v8
 
